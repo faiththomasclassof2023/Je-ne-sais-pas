4f1fdde635d1359f196eb56868e921dfd0a0aa86
Mr-Mitchell-SOCS <120413933+Mr-Mitchell-SOCS@users.noreply.github.com>
2022-12-23 10:12:37 -0500

    Getters and Setters

--- Java Snake Game/src/Food.java
+++ Java Snake Game/src/Food.java
@@ -1,5 +1,6 @@
 
 public class Food {
+	//private attributes need functions to get/set the values examples below
 	private int posX, posY, size;
 	
 	public Food (int x, int y, int size) {
@@ -7,5 +8,15 @@ public class Food {
 		posY = y;
 		int width = size;
 		int height = size;
+	}
+	
+	/*
+	public int getPosX(){
+		return posX;
+	}
+	
+	
+	public void setPosX(int num){
+		posX = num;
 	}
 }
